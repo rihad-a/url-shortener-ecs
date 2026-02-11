e8e5e4bce504f9362eb925f45a419ecbca959b03
rihad-a <Rihadwork@hotmail.com>
2026-02-11 03:44:23 +0000

    Testing - Adding in ECS task role and task execution role,  and a cloudwatch log group

--- terraform/deployment/modules/ecs/main.tf
+++ terraform/deployment/modules/ecs/main.tf
@@ -1,17 +1,71 @@
-# Creating an ECS cluster and attaching execution role
+# Creating an ECS cluster and attaching task and task execution role
 
 resource "aws_ecs_cluster" "ecs-project" {
   name = "ecs-project"
 }
 
-data "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+    name                  = "ecsTaskExecutionRole"
+    assume_role_policy    = data.aws_iam_policy_document.assume-role-policy.json
+
+    depends_on = [data.aws_iam_policy_document.assume-role-policy]
 }
 
-resource "aws_iam_role_policy_attachment" "ecsTaskRole_policy" {
-    role                  = data.aws_iam_role.ecs_task_execution_role.name
-    policy_arn            = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+
+data "aws_iam_policy_document" "assume-role-policy" {
+  statement {
+    actions               = ["sts:AssumeRole"]
+
+    principals {
+      type                = "Service"
+      identifiers         = ["ecs-tasks.amazonaws.com"]
+    }
+  }
 }
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole-policy" {
+    role                  = aws_iam_role.ecsTaskExecutionRole.name
+    policy_arn            = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role" "ecsTaskRole" {
+    name                  = "ecsTaskRole"
+    assume_role_policy    = data.aws_iam_policy_document.assume-role-policy.json   
+
+    depends_on = [data.aws_iam_policy_document.assume-role-policy]
+}
+
+data "aws_iam_policy_document" "dynamodb-policy" {
+    statement {
+      effect = "Allow"
+      actions = [
+        "dynamodb:GetItem",
+        "dynamodb:PutItem"
+      ]
+      resources = [
+        "arn:aws:dynamodb:eu-west-2:291759414346:table/url-shortener"
+        ]
+    }
+
+}
+
+resource "aws_iam_policy" "dynamodb-policy" {
+  name   = "dynamodb-policy"
+  policy = data.aws_iam_policy_document.dynamodb-policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskRole-policy" {
+    role                  = aws_iam_role.ecsTaskRole.name
+    policy_arn            = aws_iam_policy.dynamodb-policy.arn
+}
+
+# Creating a CloudWatch log group
+
+resource "aws_cloudwatch_log_group" "ecs" {
+  name = "ecs"
+
+}
+
 
 # Assigning a task definition
 
@@ -21,7 +75,11 @@ resource "aws_ecs_task_definition" "ecs-docker" {
   network_mode             = "awsvpc"
   cpu                      = var.ecs-cpu
   memory                   = var.ecs-memory
-  execution_role_arn       = "${data.aws_iam_role.ecs_task_execution_role.arn}"
+  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+  task_role_arn            = aws_iam_role.ecsTaskRole.arn
+  
+  depends_on = [ aws_cloudwatch_log_group.ecs ]
+  
   container_definitions    = jsonencode([
     {
       name      = var.ecs-container-name
@@ -32,6 +90,18 @@ resource "aws_ecs_task_definition" "ecs-docker" {
           containerPort = var.ecs-dockerport
           hostPort      = var.ecs-dockerport
         }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = aws_cloudwatch_log_group.ecs.name
+          "awslogs-region"        = "eu-west-2"
+          "awslogs-stream-prefix" = "ecs"
+        }
+      }
+      environment = [
+        {"name": "TABLE_NAME", "value": "url-shortener"},
+        {"name": "AWS_DEFAULT_REGION", "value": "eu-west-2"}
       ]
     }
   ])
